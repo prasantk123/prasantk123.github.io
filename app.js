95959f37cd0e53b3f10a02de2c04d5fe1586df18
Prasant kumar <43747177+prasantk123@users.noreply.github.com>
2020-12-07 21:55:45 -0800

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+// Ooopppss sorry i forgot to write code on javascript //
+// but no problem this website no need javascript //
+//if you want complex functionality then you would try //
+//hope you enjoy//
